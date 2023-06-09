b058abcc3dabb9d578233acb9c2cb8642664f559
Yasin Rezvani <yasinrezvani2050@gmail.com>
2023-06-09 19:50:48 +0330

    create initial variable

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 
 using namespace std;
 
+char matrix[3][3] = {'1','2','3','4','5','6','7','8','9'};
+char player = 'X';
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    
     return 0;
 }
